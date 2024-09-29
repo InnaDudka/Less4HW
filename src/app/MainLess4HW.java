6f1bb197bb3f6c7ef0ccab7ce42c6d9a4687d512
InnaDudka <innadudka1@gmail.com>
2024-09-29 13:58:38 +0300

    First commit

---
+++ src/app/MainLess4HW.java
@@ -0,0 +1,27 @@
+package app;
+
+public class MainLess4HW {
+
+    public static void main(String[] args) {
+        String finishSymbol = ".";
+        String product = "Product No ";
+        int prodNum1 = 1;
+        String smart = ": smartphone,";
+        String total = "total sales for ";
+        int dayNum1 = 5;
+        String is = " is EUR ";
+        String sales ="sales by day";
+        String days = " day ";
+        double sum = 12153.41;
+        double divide = sum / dayNum1;
+        System.out.println(product + prodNum1 + smart + "\n" +
+                total + dayNum1 + days + is + sum + "\n" +
+                sales + is + divide + finishSymbol);
+
+
+
+    }
+
+
+
+}
